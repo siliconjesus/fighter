29590dbf3e2db525c61f3866e4a1e444d036ee19
Jon Mentzell <jmentzell@redhat.com>
2022-07-14 12:11:27 -0400

    continued cleanup

--- cpp/v2/fighter.cpp
+++ cpp/v2/fighter.cpp
@@ -4,24 +4,60 @@
 
 using namespace std;
 
+class Fighter {
+    private:
+        int hitpoints;
+        char player_name[32]; // TODO make this a linked list instead
+    public:
+        Fighter() { // This is the constructor - it runs the when the object is created from this class
+            hitpoints = 60;
+        }
+        int attack (void) {
+            int attack_hp = (rand() %6 + 1);
+            if ((hitpoints - attack_hp) < 0) {
+                cout << player_name << " has been knocked out!\r\n";
+                return(5);
+            }
+            else {
+                cout << player_name << " has been hit for " << attack_hp << " points!\r\n" ; 
+            }
+            hitpoints = (hitpoints - attack_hp);
+            cout << player_name << " has " << hitpoints << " HP left!\r\n";
+            return(1);
+        }
+        void set_name (void) {
+            cout << "Enter the name of the character: ";
+            cin >> player_name; // TODO check for contents / length
+        }
+        int get_hp (void) {
+            return hitpoints;
+        }
+};
+
+int coinflip (void) {
+    int coin = (rand() %2 + 1);
+    return coin;
+};
+
 int main (void) {
-    int fighter1 = 60;
-    int fighter2 = 60;
-    int dmg = 0;
-    srand(time(NULL)); // Seed rand
 
-    while(fighter1 > 0 and fighter2 > 0) {// The fighter is still alive
-        dmg = rand() % 6 + 1;
-        cout << "Damage: " << dmg << "\r\n";
-        fighter1 = fighter1 - dmg;
+    srand(time(NULL)); // Seed the random functions
 
-        dmg = rand() % 6 + 1;
-        cout << "Damage: " << dmg << "\r\n";
-        fighter2 = fighter2 - dmg;
+    Fighter Bob;
+    Fighter Alice;
 
-        cout << "1: " << fighter1 << "\r\n";
-        cout << "2: " << fighter2 << "\r\n";
+    Bob.set_name();
+    Alice.set_name();
 
-        cout << "============="  << "\r\n";
+    while (Bob.get_hp() > 0 and Alice.get_hp() > 0) { // potentially change to looking for the '5' return code.  TBD
+        int coin = coinflip();
+        if (coin == 1) {
+            Bob.attack();
+            Alice.attack();
+        } else {
+            Alice.attack();
+            Bob.attack();
+        }
     }
+    return(0);
 }
