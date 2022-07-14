2b6e9396cef79714cf767ba6fd5a903c592f47ef
Jon Mentzell <jmentzell@redhat.com>
2022-07-13 23:54:00 -0400

    Initial entry

---
+++ README.md
@@ -0,0 +1,2 @@
+Fighter game I recreate in langages when I'm bored or learning a new 
+programming language
---
+++ cpp/fighter.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <random>
+#include <time.h>
+
+using namespace std;
+
+int main (void) {
+    int fighter1 = 60;
+    int fighter2 = 60;
+    int dmg = 0;
+    srand(time(NULL));
+
+    while(fighter1 > 0 and fighter2 > 0) {// The fighter is still alive
+        dmg = rand() % 6 + 1;
+        cout << "Damage: " << dmg << "\r\n";
+        fighter1 = fighter1 - dmg;
+
+        dmg = rand() % 6 + 1;
+        cout << "Damage: " << dmg << "\r\n";
+        fighter2 = fighter2 - dmg;
+
+        cout << "1: " << fighter1 << "\r\n";
+        cout << "2: " << fighter2 << "\r\n";
+
+        cout << "============="  << "\r\n";
+    }
+}
